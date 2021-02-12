5b9249c838e71fcce863e1609440cbc448d65444
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-02-12 08:49:21 -0300

    predicate

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -13,18 +13,15 @@ namespace delegates
             list.Add(new Product("TV", 900.00));
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("HD Case", 80.90));
 
-            list.Sort(compareProducts);
+            list.RemoveAll(p => p.Price >= 100.0);
 
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
-        }
-
-        static int compareProducts(Product p1, Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
     }
 }
